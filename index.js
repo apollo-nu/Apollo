b00642725e9e43f0aca5910236c1ddbf1a66a2fb
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 11:25:30 -0500

    debug

--- index.js
+++ index.js
@@ -5,22 +5,14 @@ const url = require("url");
 const proxy = require("express-http-proxy");
 
 const apiProxy = proxy("https://apollo-nu-server.herokuapp.com", {
-    proxyReqPathResolver: req => {
-        console.log(req.originalUrl);
-        console.log(url.parse(req.originalUrl).path);
-        return url.parse(req.originalUrl).path
-    }
+    proxyReqPathResolver: req => url.parse(req.originalUrl).path
 });
 
 const clientProxy = proxy("https://apollo-nu.herokuapp.com", {
-    proxyReqPathResolver: req => {
-        console.log(req.originalUrl);
-        console.log(url.parse(req.originalUrl).path);
-        return url.parse(req.originalUrl).path
-    }
+    proxyReqPathResolver: req => url.parse(req.originalUrl).path
 });
 
-app.use("/api/", apiProxy);
-app.use("/", clientProxy);
+app.use("/api/*", apiProxy);
+app.use("/client/*", clientProxy);
 
 app.listen(process.env.PORT || 9000);
