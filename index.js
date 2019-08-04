0f5715c7d56fc0587f526ba1884bd7e1a2abfe64
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 11:10:54 -0500

    Removed trailing slash from proxy routes

--- index.js
+++ index.js
@@ -4,20 +4,11 @@ const app = express();
 const url = require("url");
 const proxy = require("express-http-proxy");
 
-app.use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-})
-
-
-const apiProxy = proxy("https://apollo-nu-server.herokuapp.com/", {
+const apiProxy = proxy("https://apollo-nu-server.herokuapp.com", {
     proxyReqPathResolver: req => url.parse(req.originalUrl).path
 });
 
-const clientProxy = proxy("https://apollo-nu.herokuapp.com/", {
+const clientProxy = proxy("https://apollo-nu.herokuapp.com", {
     proxyReqPathResolver: req => url.parse(req.originalUrl).path
 });
 
