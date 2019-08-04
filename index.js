43ab25eda1528c2f47665205753089bd2d19a0f4
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 11:42:59 -0500

    Trying http-proxy

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
 
-const proxy = require("http-proxy-middleware");
+const url = require("url");
+const proxy = require("http-proxy");
+const apiProxy = proxy.createProxyServer();
 
-app.use("/api", proxy({
-    target: "http://apollo-nu-server.herokuapp.com",
-    changeOrigin: true
-}));
+app.use("/api", (req, res) => {
+    req.url = req.originalUrl;
+    apiProxy.web(req, res, {
+        target: {
+            host: "https://apollo-nu-server.herokuapp.com"
+        }
+    })
+});
 
-app.use("/", proxy({
-    target: "http://apollo-nu.herokuapp.com",
-    changeOrigin: true
-}));
+app.use("/", (req, res) => {
+    req.url = req.originalUrl;
+    apiProxy.web(req, res, {
+        target: {
+            host: "https://apollo-nu.herokuapp.com"
+        }
+    })
+});
 
 app.listen(process.env.PORT || 9000);
