0fa778cc6492948b4bb26cf87b0bd5c46b1351cf
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 11:01:59 -0500

    Added CORS

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 const url = require("url");
 const proxy = require("express-http-proxy");
 
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+})
+
+
 const apiProxy = proxy("https://apollo-nu-server.herokuapp.com/", {
     proxyReqPathResolver: req => url.parse(req.originalUrl).path
 });
