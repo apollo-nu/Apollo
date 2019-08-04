20e15f5639ab4c4b85db6a38b6301147073265e0
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 10:25:29 -0500

    Initial Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const url = require("url");
+const proxy = require("express-http-proxy");
+
+const apiProxy = proxy("https://apollo-nu-server.herokuapp.com/", {
+    proxyReqPathResolver: req => url.parse(req.originalUrl).path
+});
+
+const clientProxy = proxy("https://apollo-nu.herokuapp.com/", {
+    proxyReqPathResolver: req => url.parse(req.originalUrl).path
+});
+
+app.use("/api/*", apiProxy);
+app.use("/*", clientProxy);
+
+app.listen(process.env.PORT || 9000);
